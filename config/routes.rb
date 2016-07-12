b0821abfa083954c95f5f19319ab9550fd2d9a03
luissimo <khijbar@gmail.com>
2016-07-12 18:11:39 +0200

    rework layout + klanten done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
-
-  resources :customers
-  resources :companies
+  resources :customers, path: :klanten
+  resources :companies, path: :instellingen
 
  root 'pages#profiel'
-
 
 
 ## https://vast-fortress-16771.herokuapp.com/ | https://git.heroku.com/vast-fortress-16771.git
