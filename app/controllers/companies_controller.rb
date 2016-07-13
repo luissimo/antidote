ce0e4101f2462e012aa67b59be29afe34b0ff8a4
luissimo <khijbar@gmail.com>
2016-07-13 21:34:01 +0200

    instellingen finished

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -71,4 +71,7 @@ class CompaniesController < ApplicationController
     def company_params
       params.require(:company).permit(:company_name, :address_line_1, :address_line_2, :zip_code, :city_name, :country_name, :email_address, :phone_number, :kvk_number, :btw_number, :iban_number, :bic_number, :bank_holder_name)
     end
+
+
+
 end
