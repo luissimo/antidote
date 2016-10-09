74972790410990aa179f260c17851abb2e22a565
luissimo <khijbar@gmail.com>
2016-10-09 16:48:28 +0200

    minor fix

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,7 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [150,150]
+  process resize_to_limit: [120,120]
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
