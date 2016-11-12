de5dce6ef7d75857981e7a24a2c60dce4e6b49c5
luissimo <khijbar@gmail.com>
2016-11-12 13:57:48 +0100

    devise routes edited

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   resources :companies, path: :sjablonen
   resources :contacts, only: [:new, :create], via: [:post]
   devise_for :users, path: :gebruiker
+  devise_scope :user do
+    get 'gebruiker/inloggen'                => 'devise/sessions#new'
+    get 'gebruiker/registreren'             => 'devise/registrations#new'
+    get 'gebruiker/bevestigingsinstructies' => 'devise/confirmations#new'
+    get 'gebruiker/nieuwe-wachtwoord'       => 'devise/passwords#new'
+  end
 
   root                                 'oqba#index'
   match 'over-ons'                 =>  'oqba#about_us', via: [:get, :post]
